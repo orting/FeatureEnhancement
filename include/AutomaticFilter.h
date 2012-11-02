3f3e5d6d0b3719115011e30f8ccc96ff7f2c9b5e
Silas Ørting <silas.orting@gmail.com>
2012-11-03 00:29:16 +0100

    Moved fissure measures to FeatureMeasure

--- include/AutomaticFilter.h
+++ include/AutomaticFilter.h
@@ -1,10 +1,10 @@
 #pragma once
 #include "pechin_wrap.h"
+#include "FeatureMeassure.h"
 
 namespace feature_enhancement {
   class AutomaticFilter {
   public:
-    typedef double (AutomaticFilter::*FeatureMeassure)(double, double, double, double) const;
     AutomaticFilter();
     ~AutomaticFilter(){};
 
@@ -21,9 +21,9 @@ namespace feature_enhancement {
 
     void set_featureness(FeatureMeassure feature_meassure);
 
-    double fissureness_rikxoort(double voxel_value, double eig1, double eig2, double eig3) const;
-    double fissureness_lassen(double voxel_value, double eig1, double eig2, double eig3) const;
-    double fissureness_rikxoort_lassen(double voxel_value, double eig1, double eig2, double eig3) const;
+    //    double fissureness_rikxoort(double voxel_value, double eig1, double eig2, double eig3) const;
+    //    double fissureness_lassen(double voxel_value, double eig1, double eig2, double eig3) const;
+    //    double fissureness_rikxoort_lassen(double voxel_value, double eig1, double eig2, double eig3) const;
 
   private:
     void apply_no_fft(cimg_library::CImg<short> &volume,
