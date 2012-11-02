fb54a61e9bd368277661a8965d0a75e01aad1b76
Silas Ørting <silas.orting@gmail.com>
2012-11-03 00:35:17 +0100

    No longer needed

--- include/FeaturenessMeassure.h
+++
@@ -1,5 +0,0 @@
-
-
-double fissureness_rikxoort(double voxel_value, double eig1, double eig2, double eig3);
-double fissureness_lassen(double voxel_value, double eig1, double eig2, double eig3);
-double fissureness_rikxoort_lassen(double voxel_value, double eig1, double eig2, double eig3);
