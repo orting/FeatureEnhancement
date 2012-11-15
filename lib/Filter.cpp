aaca36a20be9587ef1e6f5591753d0aaa082d88a
Silas Ørting <silas.orting@gmail.com>
2012-11-16 00:31:55 +0100

    Fixed bug in kernel causing empty kernels to be created

--- lib/Filter.cpp
+++ lib/Filter.cpp
@@ -1,63 +1,33 @@
 #include <iostream>
-#include <math.h>
 #include "Filter.h"
 
+namespace feature_enhancement {
+  void kernel(Filter3D filter, int scale, Volume &out) {
+    size_t w = static_cast<size_t>(6 * scale + 1);
+    size_t k_w = w > out.width ? out.width : w;
+    size_t k_h = w > out.height ? out.height : w;
+    size_t k_d = w > out.depth ? out.depth : w;
+    size_t half_w = out.width / 2;
+    size_t half_h = out.height / 2;
+    size_t half_d = out.depth / 2;
+    size_t start_i = half_w - k_w / 2;
+    size_t start_j = half_h - k_h / 2;
+    size_t start_k = half_d - k_d / 2;
+    size_t end_i = start_i + k_w;
+    size_t end_j = start_j + k_h;
+    size_t end_k = start_k + k_d;
 
-
-void Filter::apply(cimg_library::CImg<double> &volume, int w, int h, int d, int scale) {
-  cimg_library::Cimg<double> filter(2*(w/2 +1), h, d, 1, 0);
-  double *in = filter.data();
-  fftw_complex *out = (fftw_complex *) filter.data();
-  fftw_plan forward = fftw_plan_dft_r2c_3d(d, h, w, in, out, FFTW_ESTIMATE);
-
-  int wc = w/2;
-  int hc = h/2;
-  int dc = c/2;
-  int window = 3 * scale; // We do ± 3 standard deviations
-  int start_x = wc - window > 0 ? - window : - wc;
-  int start_y = hc - window > 0 ? - window : - hc;
-  int start_z = dc - window > 0 ? - window : - dc;
-  int end_x = wc + window < w ? window : wc;
-  int end_y = hc + window < h ? window : hc;
-  int end_z = dc + window < d ? window : dc;
-
-  for (int x = start_x, i = wc + start_x; x <= end_x; ++x, ++i) {
-    for (int y = start_y, j = hc + start_y; y <= end_y; ++y, ++j) {
-      for (int z = start_z, k = dc + start_z; z <= end_z; ++z, ++k) {
-	filter(i, j, k) = this->current_filter(x,y,z);
+    double x = - static_cast<double>(k_w/2);
+    double start_y = - static_cast<double>(k_h/2);
+    double start_z = - static_cast<double>(k_d/2);
+    for (size_t i = start_i; i < end_i; ++i, ++x) {
+      double y = start_y;
+      for (size_t j = start_j; j < end_j; ++j, ++y) {
+	double z = start_z;
+	for (size_t k = start_k; k < end_k; ++k, ++z) {
+	  out(i, j, k) = filter(x, y, z, scale);
+	}
       }
     }
   }
-    
-  fftw_execute(forward);
-
-  cimg_forXYZ(volume, x, y, z) {
-    volume(x,y,z) *= filter(x,y,z);
-  }
-
-  fftw_destroy_plan(forward);
 }
-
-
-// Create the gaussfilter, shold perhaps be saved and loaded ?
-static double const normalise = 0.06349363593424098; // = 1 / (sqrt(2*pi)^3)
-double const k = normalise * (1.0 / (scale * scale * scale));
-double const s2 = 2 * scale * scale;
-cimg_library::CImg<double> filter
-
-
-
-return 
-this->normalise * 
-this->scale_frac_pow3 * 
-exp((-(x*x) - (y*y) - (z*z) ) / this->scale_pow2_2);  
-}
-void apply_dx_freq(cimg_library::CImg<double> volume, int scale);
-void apply_dy_freq(cimg_library::CImg<double> volume, int scale);
-void apply_dz_freq(cimg_library::CImg<double> volume, int scale);
-void apply_dxx_freq(cimg_library::CImg<double> volume, int scale);
-void apply_dxy_freq(cimg_library::CImg<double> volume, int scale);
-void apply_dxz_freq(cimg_library::CImg<double> volume, int scale);
-void apply_dyy_freq(cimg_library::CImg<double> volume, int scale);
-void apply_dyz_freq(cimg_library::CImg<double> volume, int scale);
-void apply_dzz_freq(cimg_library::CImg<double> volume, int scale);
